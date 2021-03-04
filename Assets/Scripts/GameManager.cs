6e1e4829e4fbf3eee553675a014dae0b0d345d94
Greg Merz <gmerz4737@sdsu.edu>
2021-03-04 10:51:16 -0800

    Tested button for functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    PlayerManager playerManager;
+    public bool isCharacterMade = false;
+
+    private void Awake() {
+        playerManager.GetComponent<PlayerManager>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("THis is sick");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LoadScene(int level) {
+        SceneManager.LoadScene(level);
+    }
+
+    public void onRollCharacterClicked() {
+        Debug.Log("Roll Character button clicked");
+        LoadScene(1);
+    }
+
+    public void onPlayGameClicked() {
+        Debug.Log("Play Game button clicked");
+
+        if (isCharacterMade) {
+            LoadScene(2);
+        }
+
+        Debug.Log("Press Roll Character first");
+    }
+
+    public void onQuitClicked() {
+        Debug.Log("Quit button clicked");
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        #else
+        Application.Quit();
+        #endif
     }
 }
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class PlayerManager : MonoBehaviour
+{
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    */
+   
+    public class Player {
+        // Player Info
+        public string name;
+        public string race;
+        public string playerClass;
+        public string allignment;
+
+        // Abilities
+        public int strength;
+        public int dexterity;
+        public int constitution;
+        public int intellignece;
+        public int wisdom; 
+        public int charisma;
+
+        // Player Stats
+        public int currXp;
+        public int maxXp;
+        public int currHp;
+        public int maxHp;
+        public int armorClass = 14;
+        public float walkingSpeed;
+        public float runningSpeed;
+        public float jumpHeight;
+        public List<string> itemList;
+    }
+
+    public static PlayerManager _instance;
+    public Player player;
+
+    void Awake() {
+        if (_instance != null && _instance != this) {
+            Destroy(this.gameObject);
+        }
+        
+        else {
+            DontDestroyOnLoad(this.gameObject);
+            _instance = this;
+            player = new Player();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
